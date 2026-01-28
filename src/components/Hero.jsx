0c34b21daad9c8ae6e943d93dbf024dfe0260915
Macaron33-T <tarm3521@gmail.com>
2026-01-28 21:06:52 +0700

    Fix VDO and Add animation scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { useState, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import { TiLocationArrow } from "react-icons/ti";
+import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -9,39 +14,99 @@ const Hero = () => {
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 4;
-  const nextVDORef = useRef(null);
+
+  const miniVideoRef = useRef(null);
+  const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
   };
 
+  useEffect(() => {
+    if (loadedVideos >= 2) {
+      setIsLoading(false);
+    }
+  }, [loadedVideos]);
+
   const upcomingVDOIndex = (currentIndex % totalVideos) + 1;
 
   const handleMiniVidClick = () => {
     setHasClicked(true);
-
     setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
   };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    },
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 88% 90%, 0% 95%)",
+      borderRadius: "0 0 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   const getVideosrc = (index) => `videos/VDO-${index}.mp4`;
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div
-            className="mask-clip-path absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-  z-50 size-64 cursor-pointer overflow-hidden rounded-lg"
-          >
+          <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div
               onClick={handleMiniVidClick}
               className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
             >
               <video
-                ref={nextVDORef}
+                ref={miniVideoRef}
                 src={getVideosrc(upcomingVDOIndex)}
                 loop
                 muted
@@ -53,48 +118,51 @@ const Hero = () => {
           </div>
 
           <video
-            ref={nextVDORef}
+            ref={nextVideoRef}
             src={getVideosrc(currentIndex)}
             loop
             muted
             id="next-video"
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 invisible absolute z-20 size-64 object-cover object-center"
+            className="absolute-center invisible z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
           />
+
           <video
-            src={getVideosrc(
-              currentIndex === totalVideos - 1 ? 1 : currentIndex,
-            )}
-            //autoPlay
+            src={getVideosrc(currentIndex === totalVideos ? 1 : currentIndex)}
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
         </div>
+
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
           Project Moon
         </h1>
+
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
               K<b>J</b>H Supremancy
             </h1>
-            <div className="absolute left-0 top-0 z-40 size-full">
-              <div className="mt-24 px-5 sm:px-10">
-                <h1 className="special-font hero-heading text-blue-100">
-                  K<b>J</b>H
-                </h1>
-                <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-                  GLORY TO PROJECT MOON!
-                  <br />
-                  PM fans can't read why i need to write it here?
-                </p>
-                <Button />
-              </div>
-            </div>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              GLORY TO PROJECT MOON! <br />
+              PM fans can't read, why I need to write it here?
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
           </div>
         </div>
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        Project Moon
+      </h1>
     </div>
   );
 };
